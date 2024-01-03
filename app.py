2736b6505a18e3e4e8f6db4af4dec24e326829df
offfahad <mughalfahad544@gmail.com>
2024-01-03 13:26:40 +0500

    code-updated

--- app.py
+++ app.py
@@ -2,14 +2,17 @@
 from flask import Flask, escape, request, render_template
 import pickle
 from sklearn.preprocessing import StandardScaler
+
 app = Flask(__name__)
 
 model = pickle.load(open('songs_hit_predictor.pkl', 'rb'))
+scaler = StandardScaler()
 
-@app.route('/', methods = ['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-
+        """
         Dancibilty = float(request.form['Dancibilty'])
         energy = float(request.form['energy'])
         key = float(request.form['key'])
@@ -26,19 +29,34 @@ def predict():
         chorus_hit = float(request.form['chorus_hit'])
         sections = float(request.form['sections'])
         decade = float(request.form['decade'])
-        
-        prediction = model.predict([[Dancibilty,energy,key,loudness,mode,speechiness,acousticness,instrumentalness,liveness,valence,tempo,duration_ms,time_signature,chorus_hit,sections,decade]])
+
+        user_inputs = [Dancibilty, energy, key, loudness, mode, speechiness, instrumentalness, liveness, valence, tempo, duration_ms, time_signature, chorus_hit, sections, decade]
+        """
+
+        # Extract user inputs
+        features = ['Dancibilty', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+                    'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature',
+                    'chorus_hit', 'sections', 'decade']
+
+        user_inputs = [float(request.form[feature]) for feature in features]
+
+        # Scale the user inputs using the pre-fitted scaler
+        scaled_inputs = scaler.fit_transform([user_inputs])
+
+        prediction = model.predict(scaled_inputs)
 
         model_result = prediction
+        print("Model Prediction: ",model_result)
         if model_result == 1:
             hit = "Hit"
         else:
             hit = "Not Hit"
 
-        return render_template("index.html", prediction_text = "Music will {}".format(hit))
+        return render_template("index.html", prediction_text="Music will {}".format(hit))
     else:
         return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
